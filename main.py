a43564d9a319c8994b1979e2f8c345889a30c47b
Luis Vereau <luis.vereau@pentago.com.br>
2023-02-22 00:17:15 -0300

    add a prediction example

--- main.py
+++ main.py
@@ -11,19 +11,28 @@ faces_dir = main_dir+'\\faces'
 
 dataSet_aux = getFaces(faces_dir)
 
-X = list()
-y = list()
-for data in dataSet_aux:
-   X.append(cv2.imread(data[0]))
-   y.append(data[1])
+X = [cv2.imread(data[0]) for data in dataSet_aux]
+y = [data[1] for data in dataSet_aux]
 
 X = np.asarray(X)
 y = np.asarray(y)
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=randint(0, 10000))
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=randint(0, 10000))
 
 model = trainModel(X_train, y_train)
 
+prediction = model.predict(X_test)
+
 test_evaluation = model.evaluate(X_test, y_test)
 
+seed = randint(0, len(X_test))
+
+print('-'*60)
+print(f'Classe real IMG de teste (escolhida aleatoriamente): {y_test[seed]}')
+print(f'Classe predita dessa IMG de teste: {np.argmax(prediction[seed])}')
 print(f'A taxa de acerto do modelo foi de {test_evaluation[1]*100:.4f}%')
+print('-'*60)
+
+cv2.imshow('IMG de teste', X_test[seed])
+cv2.waitKey(0)
+cv2.destroyAllWindows()
